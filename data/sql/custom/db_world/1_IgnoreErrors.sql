2805b922140397dc45aa0fa5cc0d098f7aa88f66
Ryan Turner <16946913+TheSCREWEDSoftware@users.noreply.github.com>
2025-12-20 22:08:03 +0000

    Remove multiple delete statements from SQL script

--- data/sql/custom/db_world/1_IgnoreErrors.sql
+++ data/sql/custom/db_world/1_IgnoreErrors.sql
@@ -15,3 +15,32 @@ DELETE FROM `waypoint_data`;
 DELETE FROM `waypoint_scripts`;
 -- Removes any SmartAI or C++ scripts from the creatures.
 UPDATE `creature_template` SET `AIName` = "", `ScriptName` = "";
+-- Delete all content of "game_event_creature" table.
+DELETE FROM `game_event_creature`;
+-- Delete all content of "game_event_creature_quest" table.
+DELETE FROM `game_event_creature_quest`;
+-- Delete all content of "game_event_creature" table.
+DELETE FROM `game_event_gameobject`;
+-- Delete all content of "game_event_creature_quest" table.
+DELETE FROM `game_event_gameobject_quest`;
+-- Delete all content of "game_event_npcflag" table.
+DELETE FROM `game_event_npcflag`;
+-- Delete all content of "game_event_npcflag_vendor" table.
+DELETE FROM `game_event_npcflag_vendor`;
+-- Delete all content of "game_event_npcflag_vendor" table.
+DELETE FROM `game_event_npcflag_vendor`;
+-- Delete all content of "game_event_pool" table.
+DELETE FROM `game_event_pool`;
+-- Delete all content of "pool_creature" table.
+DELETE FROM `pool_creature`;
+-- Delete all content of "pool_gameobject" table.
+DELETE FROM `pool_gameobject`;
+-- Delete all content of "pool_pool" table.
+DELETE FROM `pool_pool`;
+-- Delete all content of "pool_quest" table.
+DELETE FROM `pool_quest`;
+-- Delete all content of "pool_template" table.
+DELETE FROM `pool_template`;
+-- Delete all content of "smart_scripts" table.
+DELETE FROM `smart_scripts`;
+
